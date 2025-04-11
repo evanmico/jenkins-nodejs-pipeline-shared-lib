823089810978d87aa63813b83ef1d899a7c3523e
evanmico <evanmico222@gmail.com>
2025-04-11 18:21:11 -0400

    added loadGlobalConfigs function and global-configs.yaml

--- src/cicd/config/DeploymentConfigs.groovy
+++ src/cicd/config/DeploymentConfigs.groovy
@@ -1,5 +1,19 @@
 package cicd
 
 class DeploymentConfigs {
-    
+
+    public static def jobInstance = null
+
+    /*
+    configuration from jenkins-lib <b>resources/global-configs.yaml</b>
+    */
+    public static Map globalConfigs = null
+
+    public static Map loadGlobalConfigs() {
+        final String globalConfigsResource = DeploymentConfigs.jobInstance.libraryResource('global-configs.yaml')
+        DeploymentConfigs.globalConfigs = DeploymentConfigs.jobInstance.readYaml(text: globalConfigsResource)
+        DeploymentConfigs.jobInstance.echo('DEBUG: ' + DeploymentConfigs.globalConfigs) // TODO remove after debug
+        return DeploymentConfigs.globalConfigs
+    }
+
 }
