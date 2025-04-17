f25b62ff5c446bea944a2a409530a4a45ca03891
evanmico <evanmico222@gmail.com>
2025-04-17 00:45:36 -0400

    added functionality to only install prod dependencies to installDependencies.groovy

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -1,8 +1,10 @@
-public void call() {
-    _npmInstall()
+public void call(boolean onlyProductionDependencies = false) {
+    _npmInstall(onlyProductionDependencies)
 }
 
-private void _npmInstall() {
+private void _npmInstall(boolean onlyProductionDependencies) {
+    final String omitDevFlag = onlyProductionDependencies ? '--omit=dev' : ''
+
     // if npm ci doesn't work, use 'npm install'
-    sh(script: 'npm ci', label: 'Install npm dependencies')
+    sh(script: "npm ci ${omitDevFlag}", label: 'Install npm dependencies')
 }
